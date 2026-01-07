fe8d2ace1d508604f3019af17340b9dcf5ff4477
shawn <cbycdy2@gmail.com>
2026-01-07 12:01:11 -0700

    seo

--- src/commands.js
+++ src/commands.js
@@ -3,32 +3,16 @@
  * and registration.
  */
 
-export const AWW_COMMAND = {
-  name: 'awwww',
-  description: '귀여운 것 보여줄게',
-};
-
-export const SEX_COMMAND = {
-  name: '섹스',
-  description: '야한거 볼래?',
-};
-
-export const SEX_OLD_COMMAND = {
-  name: '섹스old',
-  description: '야한거 볼래? (옛날버전)',
-};
-
-
 export const SHAWNY_COMMAND = {
   name: '셔니',
   description: '셔니가 해줘야하는 것',
-  response: '<@851312069915574332>, 다빈이랑 놀아줘!!!',
+  response: '<@851312069915574332>, 서련이랑 놀아줘!!!',
 };
 
-export const DABIN_COMMAND = {
-  name: '다빈',
-  description: '다빈이가 해야하는 것',
-  response: '<@1271855264286638134>, 셔니에게 상납해!!!',
+export const SEO_COMMAND = {
+  name: '서련',
+  description: '서련이가 해야하는 것',
+  response: '<@1419241440118050858>, 셔니에게 상납해!!!',
 };
 
 export const DDAY_COMMAND = {
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import {
   InteractionType,
   verifyKey,
 } from 'discord-interactions';
-import { AWW_COMMAND, SEX_COMMAND, SEX_OLD_COMMAND, SHAWNY_COMMAND, DABIN_COMMAND, DDAY_COMMAND } from './commands.js';
+import { SHAWNY_COMMAND, SEO_COMMAND, DDAY_COMMAND } from './commands.js';
 import { getContentUrl } from './reddit.js';
 import { daysSinceTargetDate } from './utils.js';
 
@@ -58,42 +58,6 @@ router.post('/', async (request, env) => {
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     // Most user commands will come as `APPLICATION_COMMAND`.
     switch (interaction.data.name.toLowerCase()) {
-      case AWW_COMMAND.name.toLowerCase(): {
-        const cuteUrl = await getContentUrl('https://www.reddit.com/r/aww/hot.json');
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: cuteUrl,
-          },
-        });
-      }
-      case SEX_COMMAND.name.toLowerCase(): {
-        const { keys } = await env.VIDEO_STORAGE.list();
-        const randomKey = Math.floor(Math.random() * keys.length);
-        const content = await env.VIDEO_STORAGE.get(keys[randomKey].name, 'text');
-        const regex = /https?.*mp4/g;
-        let sexyUrls = content.match(regex);
-        sexyUrls = sexyUrls[0].split(',');
-        const random = Math.floor(Math.random() * sexyUrls.length);
-
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: sexyUrls[random].trim(),
-          },
-        });
-      }
-      case SEX_OLD_COMMAND.name.toLowerCase(): {
-        const reddits =['HotAsianGifs', 'KoreanSexy', 'NSFW_GIF', 'porn'];
-        const sexyUrl = await getContentUrl(`https://www.reddit.com/r/${reddits[Math.floor(Math.random() * reddits.length)]}/hot.json`);      
-
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: `||${sexyUrl}||`,
-          },
-        });
-      }
       case SHAWNY_COMMAND.name.toLowerCase(): {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -102,11 +66,11 @@ router.post('/', async (request, env) => {
           },
         });
       }
-      case DABIN_COMMAND.name.toLowerCase(): {
+      case SEO_COMMAND.name.toLowerCase(): {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            content: DABIN_COMMAND.response,
+            content: SEO_COMMAND.response,
           },
         });
       }
@@ -114,7 +78,7 @@ router.post('/', async (request, env) => {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            content: `${daysSinceTargetDate('2024-12-24', 'Asia/Seoul')}일째 ❤️`,
+            content: `${daysSinceTargetDate('2025-01-08', 'Asia/Seoul')}일째 ❤️`,
           },
         });
       }
